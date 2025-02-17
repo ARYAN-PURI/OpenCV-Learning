12d9f74cab95be921f56cec6bab2e972fc37d55f
ARYAN-PURI <aryanpuri600@gmail.com>
2025-02-17 16:41:17 +0530

    Implementation of Laplacian filters to find the edges in the images

---
+++ 14.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+def cov(image,mask):
+    rows,cols=image.shape
+    blank=np.zeros((rows,cols),dtype=np.int16)
+    m,n=mask.shape
+    for i in range(int(m/2),rows-int(m/2)):
+        for j in range(int(n/2),cols-int(n/2)):
+            result=0
+            for p in range(m):
+                for q in range(n):
+                    result+=mask[p][q]*image[i+(p - int(m/2))][j+(q - int(n/2))]
+            blank[i][j]=result
+    return blank
+
+def normalize(image):
+    rows,cols=image.shape
+    max=0
+    min=255
+    for i in range(rows):
+        for j in range(cols):
+            if(image[i][j]>max):
+                max=image[i][j]
+            if(image[i][j]<min):
+                min=image[i][j]
+    
+    newimage=np.zeros((rows,cols),dtype=np.uint8)
+    for i in range(rows):
+        for j in range(cols):
+            newimage[i][j]=round(255*((image[i][j]-min)/(max-min)))
+    return newimage
+
+img=cv2.imread('./Images/img1.jpg',0)
+img=cv2.resize(img,(512,512))
+mask = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+newimage=cov(img,mask)
+secondimage=normalize(newimage)
+cv2.imshow('Image1',img)
+cv2.imshow('Image3',secondimage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
